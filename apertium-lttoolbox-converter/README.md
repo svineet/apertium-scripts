cbcb68446b8fd17a06c3444a8fec7f476ffd7e09
Sai Vineet <saivineet89@gmail.com>
2016-01-15 09:34:08 +0530

    lttoolbox converted convenience stuff

--- apertium-lttoolbox-converter/README.md
+++ apertium-lttoolbox-converter/README.md
@@ -1,4 +1,4 @@
 Convert bilingual dictionary entries to lttoolbox entries
 ---------------------------------------------------------
 
-Run `./main.py`
+Run `./main.py inputfilepath`, the script will output tags to stdout so it can be piped to a file as required.
--- apertium-lttoolbox-converter/main.py
+++ apertium-lttoolbox-converter/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import sys
 
 
 TAG_FORMAT = '<s n="{}"/>'
@@ -15,7 +16,8 @@ def get_tags(entry):
     return tag_res, entry.split('.')[0].replace(' ', '<b/>')
 
 
-def rules_to_xml(lines):
+def rules_to_xml(lines, ofile):
+    olines = []
     for line in lines:
         if not line:
             continue
@@ -24,14 +26,20 @@ def rules_to_xml(lines):
         tagl, textl = get_tags(l)
         tagr, textr = get_tags(r)
 
-        print(E_TAG_FORMAT.format(textl+tagl, textr+tagr))
+        olines.append(E_TAG_FORMAT.format(textl+tagl, textr+tagr))
+    ofile.write("\n".join(olines)+"\n")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
         'input_file',
         help="Input file with list of bilingual dictionary entries")
+    parser.add_argument(
+        '--output-file', '-o',
+        help="Output file for xml")
     args = parser.parse_args()
 
     with open(args.input_file, 'r') as inputf:
-        xml = rules_to_xml(inputf.readlines())
+        xml = rules_to_xml(
+            inputf.readlines(),
+            open(args.output_file, 'w') if args.output_file else sys.stdout)
